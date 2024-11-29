dc1366a256f2f6073d0e644a7c3a22fbe594e929
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-30 00:21:40 +0300

    Add default languages data for language selection

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+import type { Language } from "@/types";
+
+export const defaultLanguages: Language[] = [
+  {
+    name: 'English',
+    code: 'EN',
+  },
+  {
+    name: 'Russian',
+    code: 'RU',
+  },
+  {
+    name: 'Turkish',
+    code: 'TR',
+  },
+];
