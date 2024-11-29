be291347587feb1f7c050ab7992b0fd7f268c218
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-30 00:22:38 +0300

    Add Navbar component to layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
