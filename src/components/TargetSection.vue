8dfbb28aa058212b1ad5f13f8d02b42815e3e64d
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-12-08 19:18:42 +0300

    Refactor language selection in SourceSection and TargetSection components; integrate LanguageSelect component and update language handling logic

--- src/components/TargetSection.vue
+++ src/components/TargetSection.vue
@@ -2,27 +2,41 @@
   <div class="w-full">
     <!-- Header -->
     <div class="flex items-center px-2">
-      <button v-for="(lang, index) in defaultLanguages" :key="index" @click="() => onLanguageSelect(lang.code)"
+      <button v-for="(lang, index) in defaultLanguages" :key="index" @click="() => onLanguageSelect(lang)"
         :class="clsx('px-3 py-3 duration-200 font-medium text-sm text-zinc-600 border-b', selectedLanguage == lang.code ? 'border-blue-600 text-blue-500 hover:bg-blue-50' : 'border-transparent hover:bg-zinc-100 hover:text-zinc-800')">
         {{ lang.name }}
+      </button>
+
+      <button @click="openSelector = !openSelector"
+        class="rounded-full size-12 hover:bg-zinc-100 flex items-center justify-center">
+        <ChevronDown class="text-zinc-500 size-5" />
       </button>
     </div>
 
     <!-- Text Area -->
-    <div>
+    <div class="relative">
       <textarea readonly :value="translatedText"
         :class="clsx('resize-none rounded-lg text-xl outline-none border border-zinc-200 px-4 py-3 shadow-sm w-full', { 'cursor-not-allowed opacity-70 bg-zinc-200': !translatedText })"
         name="source-input" id="source-input" cols="30" rows="5" placeholder="Translation">
         {{ translatedText }}
       </textarea>
+
     </div>
+    <!-- Language Select -->
+    <LanguageSelect :close="() => openSelector = false" :open="openSelector" :selectedLanguage="selectedLanguage"
+      :onLanguageSelect="onLanguageSelect" :languages="targetLanguages" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { defaultLanguages } from '@/data';
+import { targetLanguages } from '@/data';
 import type { TargetSectionProps } from '@/types';
 import clsx from 'clsx';
+import { ChevronDown } from 'lucide-vue-next';
+import { ref } from 'vue';
+import LanguageSelect from './LanguageSelect.vue';
 
 defineProps<TargetSectionProps>()
+
+const openSelector = ref<boolean>(false);
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-center justify-center gap-2.5 py-10 default-container">
-    <SourceSection :selectedLanguage="sourceLanguage" :onLanguageSelect="onSourceChange" :text="text"
-      :onTextChange="() => onTextChange" />
+  <div class="flex items-center justify-center gap-2.5 py-10 default-container relative">
+    <SourceSection :defaultLanguages="defaultSourceLangs" :selectedLanguage="sourceLanguage"
+      :onLanguageSelect="onSourceChange" :text="text" :onTextChange="() => onTextChange" />
 
-    <TargetSection :selectedLanguage="targetLanguage" :onLanguageSelect="onTargetChange" :text="translatedText"
-      :onTextChange="() => onTextChange" />
+    <TargetSection :defaultLanguages="defaultTargetLangs" :selectedLanguage="targetLanguage"
+      :onLanguageSelect="onTargetChange" :text="translatedText" :onTextChange="() => onTextChange" />
   </div>
 </template>
 
@@ -12,6 +12,7 @@
 import SourceSection from '@/components/SourceSection.vue';
 import TargetSection from '@/components/TargetSection.vue';
 import { defaultLanguages } from '@/data';
+import type { Language } from '@/types';
 import { ref } from 'vue';
 
 const text = ref('');
@@ -19,17 +20,29 @@ const sourceLanguage = ref<string>('');
 const targetLanguage = ref<string>('EN');
 const translatedText = ref('');
 
-const onSourceChange = (lang: any) => {
-  sourceLanguage.value = lang;
+const defaultSourceLangs = ref([{ code: 'EN', name: 'English' }, { code: 'RU', name: 'Russian' }, { code: 'TR', name: 'Turkish' }]);
+const defaultTargetLangs = ref([{ code: 'EN', name: 'English' }, { code: 'RU', name: 'Russian' }, { code: 'TR', name: 'Turkish' }]);
+
+const onSourceChange = (lang: Language) => {
+  sourceLanguage.value = lang.code;
   if (targetLanguage.value == sourceLanguage.value) {
     targetLanguage.value = defaultLanguages.find((lang) => lang.code != sourceLanguage.value)?.code || 'EN';
   }
+  const isInSourceLangs = defaultSourceLangs.value.some((item) => item.code === lang.code);
+  if (!isInSourceLangs) {
+    defaultSourceLangs.value[defaultSourceLangs.value.length - 1] = lang;
+  }
 }
 
-const onTargetChange = (lang: any) => {
-  targetLanguage.value = lang;
+const onTargetChange = (lang: Language) => {
+  targetLanguage.value = lang.code;
   if (targetLanguage.value == sourceLanguage.value) {
     sourceLanguage.value = defaultLanguages.find((lang) => lang.code != targetLanguage.value)?.code || 'EN';
+  }
+
+  const isInTargetLangs = defaultTargetLangs.value.some((item) => item.code === lang.code);
+  if (!isInTargetLangs) {
+    defaultTargetLangs.value[defaultTargetLangs.value.length - 1] = lang;
   }
 };
 
